4c7ca6dd499cd36fb78e2b41030640134937ecdd
Mamidala-Venkatesh-au26 <87692967+mamidala-venkateswararao-au26@users.noreply.github.com>
2024-02-27 23:48:35 +0530

    Create Concatination.py

---
+++ Day2/Datatypes/Concatination.py
@@ -0,0 +1,21 @@
+# Concatination : Concatenation: You can combine strings using the + operator.
+a="Python is"
+b="Awesome"
+c= a+ " "+ b
+print(c)
+
+# len
+a=[1,20,30,40]
+b=10
+
+print(len(a))
+print(type(a))
+
+#upper and lower case
+
+a="Python is awesome"
+b="Java is awesome"
+upper = a.upper()
+lower=b.lower()
+print(upper)
+print(lower)
