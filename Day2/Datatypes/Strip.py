876710c746c3c7f290d594e995371206ec9e562e
Mamidala-Venkatesh-au26 <87692967+mamidala-venkateswararao-au26@users.noreply.github.com>
2024-02-28 00:14:02 +0530

    Create Strip.py

---
+++ Day2/Datatypes/Strip.py
@@ -0,0 +1,20 @@
+# Replace
+Text ="Python is awesome"
+
+new = Text.replace("awesome","fantastic")
+
+print(new)
+
+# Output: Python is fantastic
+
+# Strip function is used to remove the whitesp[acxes from the beginning and end of the string.]
+text = "   Some spaces around   "
+stripped_text = text.strip("around")
+print("Stripped text:", stripped_text)
+
+# Output: Stripped text: Some spaces around
+
+str = '1111Hello1111'
+print(str.strip('1'))
+
+# Output: Hello
